ed26a2d6a956eb145f15f5975a1422db839496f6
AndrewStamper <andrew.james.stamper@gmail.com>
2021-10-26 17:08:23 +0100

    Restructured to add new files for use in future

---
+++ scanObject.py
@@ -0,0 +1,8 @@
+class NumpyImage:
+    # Wrapper around each image which is held as a 3d numpy array.
+
+    def __init__(self, width, height, rows_iter, info):
+        self.data = []
+
+    def f(self):
+        return 'hello world'
