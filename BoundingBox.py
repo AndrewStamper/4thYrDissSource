b4e2ea08d764486db68fd5b2721aad71dad5d83e
AndrewStamper <andrew.james.stamper@gmail.com>
2021-10-27 10:21:40 +0100

    seperated fuction to gauge brightness of each pixel from bound_values(). THis brightness function is now: brightness_of_pixel()

--- BoundingBox.py
+++ BoundingBox.py
@@ -14,12 +14,14 @@ def restrict_to_box(image_3d_scan, corner_top_left, corner_bottom_right):
     return restricted_image_3d_scan
 
 
+def brightness_of_pixel(image_3d_scan):
+    brightness_matrix = np.repeat([np.repeat([RGB_TO_BRIGHTNESS], image_3d_scan.shape[1], axis=0)], image_3d_scan.shape[0], axis=0)
+    brightness_image_3d_scan = np.multiply(brightness_matrix, image_3d_scan)
+    return np.sum(brightness_image_3d_scan, axis=2)
+
+
 def bound_values(image_3d_scan, brightness):
-
-    brigtness_matrix = np.repeat([np.repeat([RGB_TO_BRIGHTNESS], image_3d_scan.shape[1], axis=0)], image_3d_scan.shape[0], axis=0)
-    brightness_image_3d_scan = np.multiply(brigtness_matrix, image_3d_scan)
-    brightness_image_2d_scan = np.sum(brightness_image_3d_scan, axis=2)
-
+    brightness_image_2d_scan = brightness_of_pixel(image_3d_scan)
     bounded_section = np.greater_equal(brightness_image_2d_scan, np.full(brightness_image_2d_scan.shape, brightness)).astype('uint8')
     bounded_image_3d_scan = np.repeat(np.reshape(bounded_section * 255, (bounded_section.shape[0], bounded_section.shape[1], 1)), 3, axis=2)
 
