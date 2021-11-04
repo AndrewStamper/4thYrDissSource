3b6bc6a648b37a4c3a5e47cdd7fcc3941ff5b9d7
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-04 17:09:38 +0000

    Added new constants for different colours

--- Constants.py
+++ Constants.py
@@ -2,3 +2,10 @@ OUTPUT_FILE = "../output/"
 IMAGE_FILE = "../AlderHayUltrasounds/"
 
 RGB_TO_BRIGHTNESS = [0.21, 0.72, 0.07]
+
+RGBA_RED = [255, 0, 0, 255]
+RGBA_GREEN = [0, 255, 0, 255]
+RGBA_BLUE = [0, 0, 255, 255]
+RGBA_YELLOW = [255, 255, 0, 255]
+
+POINT_SIZE = 5
