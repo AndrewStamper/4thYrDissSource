6097e555ea1a1e665ec563db1612c0456fa622fc
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-04 17:11:14 +0000

    Modified Main to use the new Classes for scans and their annotations

--- Main.py
+++ Main.py
@@ -1,35 +1,28 @@
-from fileReader.AnnotationReader import *
-from fileReader.ScanReader import *
-from printing.ImageWriter import *
-from printing.SurfaceDrawer import *
-from BoundingBox import *
-
-SCAN_OF_INTEREST = "A001L"
+from ImageClasses.UltrasoundScan import *
+from ImageClasses.AnnotationsLine import *
 
 
-image_3d_annotations = read_image(SCAN_OF_INTEREST + "_a.png")
+SCAN_OF_INTEREST = "A080L"
 
-write_image("0_Annotations.png", image_3d_annotations)
-point1, point2 = get_green(image_3d_annotations)
-point3, point4 = get_blue(image_3d_annotations)
 
-image_3d_points = print_points_png(point1, point2, point3, point4, image_3d_annotations, 5)
-write_image("1_Points.png", image_3d_points)
+UltrasoundScan = UltrasoundScan.read_image(SCAN_OF_INTEREST + ".jpg")
+Lines = AnnotationLineScan.read_image(SCAN_OF_INTEREST + "_a.png", scan=UltrasoundScan)
+Points = Lines.find_points()
 
-image_3d_scan = read_image(SCAN_OF_INTEREST + ".jpg")
-write_image("2_Scan.png", image_3d_scan)
+Lines.write_image("0'_Annotations.png")
+Points.write_image("1'_Points.png")
+UltrasoundScan.write_image("2'_Scan.png")
+UltrasoundScan.add_annotations().write_image("3'_Scan_with_annotations.png")
 
-image_3d_scan_with_points = add_points(image_3d_scan, image_3d_points)
-write_image("3_Scan_with_annotations.png", image_3d_scan_with_points)
+corner_top_left, corner_bottom_right = NumpyImage.find_box(Points.points)
+RestrictedScan = UltrasoundScan.restrict_to_box(corner_top_left, corner_bottom_right)
 
-corner_top_left, corner_bottom_right = find_box(point1, point2, point3, point4)
-restricted_image_3d_scan = restrict_to_box(image_3d_scan, corner_top_left, corner_bottom_right)
-write_image("4_Restricted_scan.png", restricted_image_3d_scan)
+RestrictedScan.write_image("4'_Restricted_scan.png")
 
 brightness = 70
-bounded_image_3d_scan = bound_values(restricted_image_3d_scan, brightness)
-write_image("5_Bounded_scan.png", bounded_image_3d_scan)
+BoundedScan = RestrictedScan.bound_values(brightness)
+BoundedScan.write_image("5'_Bounded_scan.png")
 
-plot_as_3d('6_3D_surface_of_scan.html', brightness_of_pixel(image_3d_scan))
+UltrasoundScan.plot_as_3d("6'_3D_surface_of_scan.html")
 
 print("complete")
