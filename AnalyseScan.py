ec96ba0dbe780fc936036871cd61fa2b8f28df36
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-04 21:22:59 +0000

    moved analysing tools to new file

---
+++ AnalyseScan.py
@@ -0,0 +1,26 @@
+from ImageClasses.UltrasoundScan import UltrasoundScan
+from ImageClasses.AnnotationsLine import AnnotationLineScan
+from ImageClasses.NumpyImage import NumpyImage
+
+
+def analyse_scan(scan_number):
+    ultrasound_scan = UltrasoundScan.read_image(scan_number + ".jpg")
+    lines = AnnotationLineScan.read_image(scan_number + "_a.png", scan=ultrasound_scan)
+    points = lines.find_points()
+
+    lines.write_image("analyse_scan_0_Annotations.png")
+    points.write_image("analyse_scan_1_Points.png")
+    ultrasound_scan.write_image("analyse_scan_2_Scan.png")
+    ultrasound_scan.add_annotations().write_image("analyse_scan_3_Scan_with_annotations.png")
+
+    corner_top_left, corner_bottom_right = NumpyImage.find_box(points.points)
+    restricted_scan = ultrasound_scan.restrict_to_box(corner_top_left, corner_bottom_right)
+
+    restricted_scan.write_image("analyse_scan_4_Restricted_scan.png")
+    restricted_scan.add_annotations().write_image("analyse_scan_5_Restricted_scan_with_annotations.png")
+
+    brightness = 70
+    bounded_scan = restricted_scan.bound_values(brightness)
+    bounded_scan.write_image("analyse_scan_6_Bounded_scan.png")
+
+    ultrasound_scan.plot_as_3d("analyse_scan_7_3D_surface_of_scan.html")
