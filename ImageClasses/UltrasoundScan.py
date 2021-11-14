d3eaac84c8022c6869999d17c15fa916b748bc43
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-14 18:52:52 +0000

    added some filtering methods

--- ImageClasses/UltrasoundScan.py
+++ ImageClasses/UltrasoundScan.py
@@ -3,6 +3,7 @@ from Constants import *
 import numpy as np
 import plotly.offline as go_offline
 import plotly.graph_objects as go
+from functools import partial
 import math
 
 
@@ -56,7 +57,8 @@ class UltrasoundScan(NumpyImage):
 
         fig = go.Figure()
         fig.add_trace(go.Surface(z=self.image_3d, x=x, y=y))
-        fig.update_layout(scene=dict(aspectratio=dict(x=self.get_width()/100, y=self.get_height()/100, z=125/100), xaxis=dict(range=[0, self.get_width()],), yaxis=dict(range=[0, self.get_height()])))
+        fig.update_layout(
+            scene=dict(aspectratio=dict(x=self.get_width() / 100, y=self.get_height() / 100, z=125 / 100), xaxis=dict(range=[0, self.get_width()], ), yaxis=dict(range=[0, self.get_height()])))
         go_offline.plot(fig, filename=OUTPUT_FILE + filename, validate=True, auto_open=False)
 
     # use a bounding value to segment
@@ -85,11 +87,66 @@ class UltrasoundScan(NumpyImage):
 
         kernel = np.zeros(size)
         for i in range(0, size):
-            x = i - 1 - size/2
-            kernel[i] = (1/math.sqrt(2*math.pi*math.pow(standard_deviation, 2)))*math.exp(-(pow(x, 2)/2*pow(standard_deviation, 2)))
-        size_1_kernel = kernel/kernel.sum()
-        print(size_1_kernel)
+            x = i - 1 - size / 2
+            kernel[i] = (1 / math.sqrt(2 * math.pi * math.pow(standard_deviation, 2))) * math.exp(-(pow(x, 2) / 2 * pow(standard_deviation, 2)))
+        size_1_kernel = kernel / kernel.sum()
+        # print(size_1_kernel)
 
         filtered_once = np.apply_along_axis(lambda x: np.convolve(x, size_1_kernel, mode='same'), 0, self.image_3d)
         filtered_twice = np.apply_along_axis(lambda x: np.convolve(x, size_1_kernel, mode='same'), 1, filtered_once).astype(dtype=np.uint8)
         return UltrasoundScan(filtered_twice)
+
+    def gradient(self, size, high_val='upper_quartile', low_val='lower_quartile'):
+        high_func = {
+            'max': partial(np.amax, axis=2),
+            'mean': partial(np.mean, axis=2),
+            'median': partial(np.median, axis=2),
+            'upper_quartile': partial(np.quantile, q=0.75, axis=2)
+        }[high_val]
+        low_func = {
+            'min': partial(np.amin, axis=2),
+            'mean': partial(np.mean, axis=2),
+            'median': partial(np.median, axis=2),
+            'lower_quartile': partial(np.quantile, q=0.25, axis=2)
+        }[low_val]
+
+        v = self.sliding_window_view((size, size), padding_type='input')
+        v = np.reshape(v, (v.shape[0], v.shape[1], -1))
+
+        high = high_func(v)
+        low = low_func(v)
+        result = high-low
+        return UltrasoundScan(result)
+
+    def sliding_window_view(self, shape, step=(1, 1), array=None, padding_type='input', padding=0):
+        if array is None:
+            input_array = self.image_3d
+        else:
+            input_array = array
+
+        # TODO fix the padding indexes for even size filters
+        # TODO fix padding in output padding mode
+        # TODO increment a step feature for stride
+
+        if padding_type == 'input':
+            rows_to_add_each_side = math.floor(shape[0] / 2)
+            columns_to_add_each_side = math.floor(shape[1] / 2)
+            new_shape = (input_array.shape[0] + (rows_to_add_each_side * 2), input_array.shape[1] + (columns_to_add_each_side * 2))
+            to_stride = np.full(new_shape, padding)
+            to_stride[rows_to_add_each_side:new_shape[0] - rows_to_add_each_side, columns_to_add_each_side:new_shape[1] - columns_to_add_each_side] = input_array
+
+        elif padding_type == 'output' or padding_type == 'none':
+            to_stride = input_array
+        else:
+            raise ValueError('selected padding mode is not supported')
+
+        stride = np.lib.stride_tricks.sliding_window_view(to_stride, shape)
+
+        if padding_type == 'output':
+            stride_prime = np.full((*input_array.shape, *shape), padding)
+            print(stride_prime.shape)
+            print(stride.shape)
+            stride_prime[shape[0] - 1:stride_prime.shape[0] - shape[0] + 1, shape[1] - 1:stride_prime.shape[1] - shape[1] + 1] = input_array
+            stride = stride_prime
+
+        return stride
