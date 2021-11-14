a52d1710baf73255e9c8573900a3ac7ba5d8f2d8
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-14 21:18:22 +0000

    implemented new ultrasoundscan.gradient()

--- ExploreFiltering.py
+++ ExploreFiltering.py
@@ -1,11 +1,18 @@
-from ImageClasses.UltrasoundScan import UltrasoundScan
+from ImageClasses.Ultrasound.UltrasoundScan import UltrasoundScan
 
 
 def explore_filtering(scan_number):
     original = UltrasoundScan.read_image(scan_number + ".jpg")
     original.write_image("explore_filtering_1_Scan.png")
+
+    progression = UltrasoundScan.read_image(scan_number + ".jpg")
+
     gauss_filtered = original.gauss_filter(101, 0.7)
     gauss_filtered.write_image("explore_filtering_2_Scan.png")
-    original.add_progression(gauss_filtered)
+    progression.add_progression(gauss_filtered)
 
-    original.plot_as_3d("explore_filtering_3_3D_surface_of_scan.html")
+    gradient = original.gradient(5, 'upper_quartile', 'lower_quartile')
+    gradient.write_image("explore_filtering_3_Scan.png")
+    progression.add_progression(gradient)
+
+    progression.plot_as_3d("explore_filtering_4_3D_surface_of_scan.html")
