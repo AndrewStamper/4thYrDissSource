50764d087a018688a0a2247dd91959001664a990
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-18 16:16:04 +0000

    refactored

--- ExploreFiltering.py
+++ ExploreFiltering.py
@@ -12,21 +12,21 @@ def explore_filtering(scan_number):
     progression = UltrasoundScan.read_image(scan_number + ".jpg")
 
     gauss_filtered = original.gauss_filter(101, 0.7)
-    gauss_filtered.write_image(file_name + str(file_number) + '_original')
+    gauss_filtered.write_image(file_name + str(file_number) + 'gauss_filtered')
     file_number = file_number + 1
     progression.add_progression(gauss_filtered)
 
     gradient = original.gradient(5, 'upper_quartile', 'lower_quartile')
-    gradient.write_image(file_name + str(file_number) + '_original')
+    gradient.write_image(file_name + str(file_number) + '_gradient')
     file_number = file_number + 1
     progression.add_progression(gradient)
 
     down_sampled = original.down_sample((5, 5))
-    down_sampled.write_image(file_name + str(file_number) + '_original')
+    down_sampled.write_image(file_name + str(file_number) + '_down_sampled')
     file_number = file_number + 1
 
     up_sampled = down_sampled.up_sample((5, 5))
-    up_sampled.write_image(file_name + str(file_number) + '_original')
+    up_sampled.write_image(file_name + str(file_number) + '_up_sampled')
     file_number = file_number + 1
 
     progression.plot_as_3d("explore_filtering_6_3D_surface_of_scan.html")
