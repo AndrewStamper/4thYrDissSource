dbe14917fb5f4ee9a84edac625feb52597ddcdc9
AndrewStamper <andrew.james.stamper@gmail.com>
2021-11-19 15:53:57 +0000

    added visualise mask

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 from AnalyseScan import analyse_scan
 from ExploreFiltering import explore_filtering
+from Visualise_mask import visualise_mask
 
 scan_number = "A080L"
 
 # analyse_scan(scan_number)
 
-explore_filtering(scan_number)
+# explore_filtering(scan_number)
+
+visualise_mask(scan_number)
 
 print("complete")
