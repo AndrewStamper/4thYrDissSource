7b1fff069485ce8fab3bf5339bf57426cafdab8f
AndrewStamper <andrew.james.stamper@gmail.com>
2021-12-09 18:21:59 +0000

    updated U net to work for both classification and segmentation tasks

--- Machine_Learning/U_Net/Unet.py
+++ Machine_Learning/U_Net/Unet.py
@@ -3,21 +3,35 @@ from Machine_Learning.U_Net.Models import UnetConfig, ClassificationModel, Segme
 
 
 class Unet:
-    def __init__(self):
-        # Create an instance of the model
-        self.model = ClassificationModel()
+    def __init__(self, mode="Segmentation", s_dim=1, config=UnetConfig()):
 
-        # Define Loss
-        self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        if mode == "Segmentation":
+            # Create an instance of the model
+            self.model = SegmentationModel(dim=s_dim, config=config)
+            # Define Loss
+            self.loss_object = tf.keras.losses.CategoricalCrossentropy()
+            # Define loss and accuracy
+            self.train_loss = tf.keras.metrics.Mean(name='train_loss')
+            self.train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
+            self.validation_loss = tf.keras.metrics.Mean(name='validation_loss')
+            self.validation_accuracy = tf.keras.metrics.CategoricalAccuracy(name='validation_accuracy')
+
+        elif mode == "Classification":
+            # Create an instance of the model
+            self.model = ClassificationModel(config=config)
+            # Define Loss
+            self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+            # Define loss and accuracy
+            self.train_loss = tf.keras.metrics.Mean(name='train_loss')
+            self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+            self.validation_loss = tf.keras.metrics.Mean(name='validation_loss')
+            self.validation_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='validation_accuracy')
+
+        else:
+            raise ValueError("u_net model is not supported")
 
         # Pick optimizer
         self.optimizer = tf.keras.optimizers.Adam()
-
-        # Define loss and accuracy
-        self.train_loss = tf.keras.metrics.Mean(name='train_loss')
-        self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
-        self.validation_loss = tf.keras.metrics.Mean(name='validation_loss')
-        self.validation_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='validation_accuracy')
 
         # Record number of epochs ran
         self.epoch = 0
@@ -73,3 +87,30 @@ class Unet:
         )
         print("weights:", len(self.model.weights))
         print("trainable weights:", len(self.model.trainable_weights))
+
+    def run(self, x_train, y_train, x_test, y_test, batch=32, epochs=5):
+        y_train = y_train.reshape((y_train.shape[0], -1))
+        y_test = y_test.reshape((y_test.shape[0], -1))
+
+
+        x_train, x_test = x_train / 255.0, x_test / 255.0
+        # Add a channels dimension
+        x_train = x_train[..., tf.newaxis].astype("float32")
+        x_test = x_test[..., tf.newaxis].astype("float32")
+
+        # Shuffle the data and put into batches
+        train_ds = tf.data.Dataset.from_tensor_slices(
+            (x_train, y_train)).shuffle(10000).batch(batch)
+        test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch)
+
+        # Run for a number of Epochs
+        for epoch in range(epochs):
+            self.one_epoch(train_ds, test_ds)
+
+    def get_mask(self, x):
+        # noinspection PyCallingNonCallable
+        y = self.model(x, training=False)
+        return y.reshape((*x.shape, -1))
+
+
+
