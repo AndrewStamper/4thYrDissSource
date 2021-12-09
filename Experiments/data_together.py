8b07c4fa37a07286c42bff45e771391dc91a7dbd
AndrewStamper <andrew.james.stamper@gmail.com>
2021-12-09 18:19:22 +0000

    added some primitive testing of my new U net model

--- Experiments/data_together.py
+++ Experiments/data_together.py
@@ -1,4 +1,5 @@
 from Data.DataCollection import ScanCollection
+from Machine_Learning.U_Net.Unet import Unet, UnetConfig
 
 
 def test_data():
@@ -6,5 +7,11 @@ def test_data():
     training_data = ScanCollection(scan_numbers)
     first = training_data.get_scan("A001L")
     first.ultrasound_scan.write_image("A001L" + '_original')
+
     training_data.crop((512, 512))
     first.ultrasound_scan.write_image("A001L" + '_cropped')
+
+    x, y = training_data.load_data()
+    config = UnetConfig(tiers=1, filter_ratio=2, scale=2, num_filters=2, num_convolutions=1, convolution_size=3)
+    u_net = Unet(s_dim=1, config=config)
+    u_net.run(x, x, x, x, batch=1, epochs=1000)
