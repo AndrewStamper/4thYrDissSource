3c85b1791d4096e344801af82a259d8253f3f302
AndrewStamper <andrew.james.stamper@gmail.com>
2021-12-09 18:20:15 +0000

    Added new functions to enable use of this class for inputs to the U net

--- Data/DataCollection.py
+++ Data/DataCollection.py
@@ -5,20 +5,24 @@ import numpy as np
 
 # pair of ground truth masks and their corresponding ultrasound scan
 class _Scan:
-    def __init__(self, scan_number):
+    def __init__(self, scan_number, predictions_only=False):
         self.ultrasound_scan = UltrasoundScan(filename=(scan_number + ".jpg"))
-        self.ground_truth = MaskCollection(scan_number)
+        self.predictions_only = predictions_only
+        if not self.predictions_only:
+            self.ground_truth = MaskCollection(scan_number)
 
     def crop(self, shape):
         self.ultrasound_scan.crop(shape)
-        self.ground_truth.crop(shape)
+        if not self.predictions_only:
+            self.ground_truth.crop(shape)
 
 
 # each object of this type will be a set of scans and their corresponding masks
 class ScanCollection:
-    def __init__(self, scan_numbers):
+    def __init__(self, scan_numbers, predictions_only=False):
+        self.predictions_only = predictions_only
         self.scan_numbers = np.asarray(scan_numbers)
-        v_scans = np.vectorize(_Scan)
+        v_scans = np.vectorize(lambda a: _Scan(a, predictions_only=predictions_only))
         self.scans = v_scans(self.scan_numbers)
 
     def number_of_scans(self):
@@ -34,3 +38,13 @@ class ScanCollection:
     def crop(self, shape):
         for scan in self.scans:
             scan.crop(shape)
+
+    def load_data(self):
+        x = np.zeros((*self.scans.shape, *self.scans[0].ultrasound_scan.image_3d.shape))
+        y = np.zeros((*x.shape, 3))
+
+        for i in range(0, self.scans.shape[0]):
+            x[i, :, :] = self.scans[i].ultrasound_scan.image_3d
+            if not self.predictions_only:
+                y[i, :, :] = self.scans[i].ground_truth.as_single()
+        return x, y
