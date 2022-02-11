9e0f1c4c036e2df9de6ff61738752a5cbe42aac1
AndrewStamper <andrew.james.stamper@gmail.com>
2022-02-11 17:24:48 +0000

    updated test data to use new Ml script and seperate training and validation sets

--- Experiments/data_together.py
+++ Experiments/data_together.py
@@ -1,17 +1,33 @@
 from Data.DataCollection import ScanCollection
-from Machine_Learning.U_Net.Unet import Unet, UnetConfig
+from Segmentation.Interface import ML
+from Constants import *
 
 
 def test_data():
-    scan_numbers = ["A001L", "A001R", "A003L"]
-    training_data = ScanCollection(scan_numbers)
-    first = training_data.get_scan("A001L")
-    first.ultrasound_scan.write_image("A001L" + '_original')
+    # load data
+    training_scan_numbers = ["A001L", "A002L",
+                             "A021R", "A023R", "A024L", "A024R", "A026L",
+                             "A030L", "A032L", "A032R", "A034L",
+                             "A044R", "A045R", "A046R",
+                             "A052R", "A053L"]
+    validation_scan_numbers = ["A001R", "A003L",
+                               "A022R", "A030R", "A055L"]
 
-    training_data.crop((512, 512))
-    first.ultrasound_scan.write_image("A001L" + '_cropped')
+    # load data into my format
+    training_data = ScanCollection(training_scan_numbers)
+    validation_data = ScanCollection(validation_scan_numbers)
 
-    x, y = training_data.load_data()
-    config = UnetConfig(tiers=1, filter_ratio=2, scale=2, num_filters=2, num_convolutions=1, convolution_size=3)
-    u_net = Unet(s_dim=1, config=config)
-    u_net.run(x, x, x, x, batch=1, epochs=1000)
+    # crop data
+    training_data.crop(INPUT_SHAPE)
+    validation_data.crop(INPUT_SHAPE)
+
+    # convert data into format for tensorflow
+    train_dataset = training_data.load_data()
+    validation_dataset = validation_data.load_data()
+
+    # run segmentation model on data
+    ml = ML(train_dataset, validation_dataset)
+    ml.display_example()
+    ml.train()
+    ml.show_epoch_progression()
+    ml.display_example(predictions=True, train_data=False, num=3)
