9398be5834c8912db7c0567d922c0f3a85639e8f
AndrewStamper <andrew.james.stamper@gmail.com>
2022-03-03 17:45:01 +0000

    Updated to allow saving and loading models from disk

--- Main.py
+++ Main.py
@@ -5,16 +5,20 @@ This is the main API which calls all subsequent actions
 from Experiments.AnalyseScan import analyse_scan
 from Experiments.ExploreFiltering import explore_filtering
 from Experiments.Visualise_mask import visualise_mask
-from Experiments.data_together import test_data
+from Experiments.data_together import explore_image_segmentation
+from Experiments.loadingSavedModel import explore_reloading_models
 
 scan_number = "A080L"
 
-analyse_scan(scan_number)
+#analyse_scan(scan_number)
 
-explore_filtering(scan_number)
+#explore_filtering(scan_number)
 
-visualise_mask("A001R")
+#visualise_mask("A001R")
 
-test_data()
+#explore_image_segmentation()
+
+explore_reloading_models()
+
 
 print("complete")
