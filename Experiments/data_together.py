a01648dc38d7cf77f205bcfafaea0e495b698197
AndrewStamper <andrew.james.stamper@gmail.com>
2022-03-03 19:24:49 +0000

    Experimented using more data to train and validate on

--- Experiments/data_together.py
+++ Experiments/data_together.py
@@ -3,15 +3,39 @@ from Segmentation.Interface import ML
 from Constants import *
 
 
-def test_data():
+def explore_image_segmentation():
     # load data
-    training_scan_numbers = ["A001L", "A002L",
-                             "A021R", "A023R", "A024L", "A024R", "A026L",
-                             "A030L", "A032L", "A032R", "A034L",
-                             "A044R", "A045R", "A046R",
-                             "A052R", "A053L"]
-    validation_scan_numbers = ["A001R", "A003L",
-                               "A022R", "A030R", "A055L"]
+    # training_scan_numbers = ["A001L", "A002L",
+    #                          "A021R", "A023R", "A024L", "A024R", "A026L",
+    #                          "A030L", "A032L", "A032R", "A034L",
+    #                          "A044R", "A045R", "A046R",
+    #                          "A052R", "A053L"]
+    # validation_scan_numbers = ["A001R", "A003L",
+    #                            "A022R", "A030R", "A055L"]
+
+    training_scan_numbers = []
+    validation_scan_numbers = []
+
+    for number in range(1, 40):
+        for letter in ["L","R"]:
+            if number < 10:
+                tnumber = "0" + str(number)
+            else:
+                tnumber = str(number)
+            training_scan_numbers.append("A0" + tnumber + letter)
+
+    training_scan_numbers.remove("A005R")
+    training_scan_numbers.remove("A018R")
+
+    for number in range(40, 50):
+        for letter in ["L","R"]:
+            if number < 10:
+                tnumber = "0" + str(number)
+            else:
+                tnumber = str(number)
+            validation_scan_numbers.append("A0" + tnumber + letter)
+
+    validation_scan_numbers.remove("A041R")
 
     # load data into my format
     training_data = ScanCollection(training_scan_numbers)
@@ -28,8 +52,13 @@ def test_data():
     validation_dataset = validation_data.load_data()
 
     # run segmentation model on data
-    ml = ML(train_dataset, validation_dataset)
+    ml = ML()
+    ml.configure_training_data_pipeline(train_dataset, augmentations_type=AUGMENTATION_TYPE)
+    ml.configure_validation_data_pipeline(validation_dataset)
     ml.display_example()
     ml.train()
     ml.show_epoch_progression()
-    ml.display_example(predictions=True, train_data=False, num=3)
+    ml.display_example(predictions=True, train_data=False, num=BATCH_SIZE)
+
+    # save the model for re-use later
+    ml.save_model(filename=SAVE_MODEL_AS)
