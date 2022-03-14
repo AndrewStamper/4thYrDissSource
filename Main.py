3b1133d05046b7371b80dfeefadeeec68877452c
AndrewStamper <andrew.james.stamper@gmail.com>
2022-03-14 19:07:04 +0000

    Updated to add FHC

--- Main.py
+++ Main.py
@@ -7,18 +7,20 @@ from Experiments.ExploreFiltering import explore_filtering
 from Experiments.Visualise_mask import visualise_mask
 from Experiments.data_together import explore_image_segmentation
 from Experiments.loadingSavedModel import explore_reloading_models
+from Experiments.ExploreFHC import explore_FHC
 
 scan_number = "A080L"
 
-#analyse_scan(scan_number)
+# analyse_scan(scan_number)
 
-#explore_filtering(scan_number)
+# explore_filtering(scan_number)
 
-#visualise_mask("A001R")
+# visualise_mask("A001R")
 
-#explore_image_segmentation()
+# explore_image_segmentation()
 
-explore_reloading_models()
+# explore_reloading_models()
 
+explore_FHC("A047L")
 
 print("complete")
